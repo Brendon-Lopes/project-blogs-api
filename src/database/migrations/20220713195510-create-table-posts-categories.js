1fa84c5869d1274705a678bc2f4610c32eaacf7a
Brendon <brendon.lopes.21@gmail.com>
2022-07-13 20:59:57 -0300

    fix(migrations): :bug: fixes table name in PostCatefories migration

--- src/database/migrations/20220713195510-create-table-posts-categories.js
+++ src/database/migrations/20220713195510-create-table-posts-categories.js
@@ -27,6 +27,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PostsCategories');
+    await queryInterface.dropTable('PostCategories');
   },
 };
