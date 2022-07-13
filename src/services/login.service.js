7f5ad065fa2aa17b9f67e1a97daed3935327f3de
Brendon <brendon.lopes.21@gmail.com>
2022-07-13 19:01:24 -0300

    feat(services): :sparkles: adds services layer to /login post route

---
+++ src/services/login.service.js
@@ -0,0 +1,26 @@
+const { User } = require('../database/models');
+const httpStatusCodes = require('../helpers/httpStatusCodes');
+const jwt = require('../helpers/auth');
+
+const login = async (email, password) => {
+  const user = await User.findOne({
+    where: {
+      email,
+      password,
+    },
+  });
+
+  if (user === null) {
+    const error = new Error('Invalid fields');
+    error.status = httpStatusCodes.BAD_REQUEST;
+    throw error;
+  }
+
+  const token = jwt.createToken(email);
+
+  return token;
+};
+
+module.exports = {
+  login,
+};
