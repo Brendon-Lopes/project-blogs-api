d5efccf0238fdf238dadbe1f42b938aad862dd4c
Brendon <brendon.lopes.21@gmail.com>
2022-07-14 15:36:43 -0300

    fix(api): :bug: removed middleware from api.js that was breaking the tests

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = require('./routes');
-const middleware = require('./middlewares');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/login', router.login);
-app.use(middleware.errorHandler);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
