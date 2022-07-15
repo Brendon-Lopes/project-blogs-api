9628a2df967cd7674fd2f59cbd9d03492ca09e17
Brendon <brendon.lopes.21@gmail.com>
2022-07-15 20:51:14 -0300

    feat(api): :sparkles: adds the /post route to the api

--- src/api.js
+++ src/api.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use('/login', router.login);
 app.use('/user', router.user);
 app.use('/categories', router.categories);
+app.use('/post', router.post);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
