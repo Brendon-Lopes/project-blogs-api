182ed2c3b38e20024debe5fa4a0916927b4dd686
Brendon <brendon.lopes.21@gmail.com>
2022-07-21 18:52:56 -0300

    fix: fixes enviroment variables name

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const app = require('./api');
 
-// não remova a variável `API_PORT` ou o `listen`
-const port = process.env.API_PORT || 3000;
+// não remova a variável `PORT` ou o `listen`
+const port = process.env.PORT || 3000;
 
 // não remova esse endpoint
 app.get('/', (_request, response) => {
