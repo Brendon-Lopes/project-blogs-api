1b0409934a42b59074abb6330bba10f956789f1d
Brendon <brendon.lopes.21@gmail.com>
2022-07-21 21:00:04 -0300

    fix: fixes db name enviroment variable for heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || 'test';
+// const environment = process.env.NODE_ENV || 'test';
 
-const suffix = {
-  dev: '-dev',
-  development: '-dev',
-  test: '-test',
-};
+// const suffix = {
+//   dev: '-dev',
+//   development: '-dev',
+//   test: '-test',
+// };
 
 const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database:
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
+  database: process.env.MYSQL_DB_NAME,
+  // `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
   dialect: process.env.DB_DIALECT,
